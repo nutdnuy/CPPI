442131837357186057b2a22e4e1261a2c0e790a8
nutdnuy <46078640+nutdnuy@users.noreply.github.com>
2024-12-12 17:05:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import edhec_risk_kit_123 as erk
 import io
 import plotly.express as px
